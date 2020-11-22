3288ff175ce04f32727832cff41e22903fab8f9d
leilahashemi <36041354+leilahashemi@users.noreply.github.com>
2020-11-22 20:32:04 +0100

    Function

--- script.js
+++ script.js
@@ -1,10 +1,39 @@
-4 +3;
-if (4+3 === 7){
-	// alert("You're smart!");
-	console.log("Helooooooo");
+// function sayHello(){
+// 	console.log("Helooooooo");
+// } 
+// sayHello();
+
+// var sayBye = function(){
+// 	console.log("Bye");
+// }
+
+// sayBye();
+
+// function sing(song){
+// 	console.log(song);
+// }
+
+// sing("Laaa deeee daaa");
+// sing("helllllooooooo");
+// sing("backstreets back alright");
+
+// var a = function multiply(a,b){
+// 	// console.log(a,b);
+// 	// return a;
+// 	// return a*b;
+// 	// return b;
+// 	if (a>10 || b>10){
+// 		return "that's too hard";
+// 	}else{
+// 		return a*b;
+// 	}
+// 	return a*b;
+// }
+
+// // multiply(5,10);
+// a();
+function multiply(a,b){
+	return a*b;
 }
-console.log("Helooooooo");
-console.log("Helooooooo");
-console.log("Helooooooo");
-console.log("Helooooooo");
-console.log("Helooooooo");
+
+alert(multiply(3,4 ));
