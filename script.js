a2fa9d5e6c6afc77b366e25d28396b044dc60af0
leilahashemi <36041354+leilahashemi@users.noreply.github.com>
2020-11-22 19:13:50 +0100

    Javascript on our webpage

---
+++ script.js
@@ -0,0 +1,10 @@
+4 +3;
+if (4+3 === 7){
+	// alert("You're smart!");
+	console.log("Helooooooo");
+}
+console.log("Helooooooo");
+console.log("Helooooooo");
+console.log("Helooooooo");
+console.log("Helooooooo");
+console.log("Helooooooo");
