e866445a15291e2ca71ef3cc8d2d8a048914193f
leilahashemi <36041354+leilahashemi@users.noreply.github.com>
2020-11-23 19:29:06 +0100

    JS: Object

--- script.js
+++ script.js
@@ -1,39 +1,21 @@
-// function sayHello(){
-// 	console.log("Helooooooo");
-// } 
-// sayHello();
+var user = {
+	name: "John",
+	age: 34,
+	hobby: "Soccer",
+	isMarried: false,
+	spells: ["abrakadra", "shazam", "boo"],
+	shout: function(){
+		console.log("AHHHHHH!");
+	}
+};
 
-// var sayBye = function(){
-// 	console.log("Bye");
-// }
-
-// sayBye();
-
-// function sing(song){
-// 	console.log(song);
-// }
-
-// sing("Laaa deeee daaa");
-// sing("helllllooooooo");
-// sing("backstreets back alright");
-
-// var a = function multiply(a,b){
-// 	// console.log(a,b);
-// 	// return a;
-// 	// return a*b;
-// 	// return b;
-// 	if (a>10 || b>10){
-// 		return "that's too hard";
-// 	}else{
-// 		return a*b;
-// 	}
-// 	return a*b;
-// }
-
-// // multiply(5,10);
-// a();
-function multiply(a,b){
-	return a*b;
-}
-
-alert(multiply(3,4 ));
+var list = [
+	{
+		username: "andy";
+		password: "secret"
+	},
+	{
+		username: "jess",
+		password: "123"
+	}
+];
